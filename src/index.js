f1c881d9bd3dc3530d7b7ccb6606ddfaa4155d61
AndrewK0704 <andrew.kozhenov@mail.ru>
2022-07-23 01:40:26 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,46 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+    let str1='';
+    let str2='';
+    let k=0;
+    
+    for(let i=0; i<expr.length; i=i+10){
+        
+        if(expr[i]==='*'){
+            str1=str1+' ';            
+        } else
+        {           
+        for (let j=i; j<i+10; j++){
+            if(expr[j]!=='0'){
+            k=j;
+            j=i+10;
+            }
+        }
+      
+        for(;k<i+10; k=k+2){
+            if (expr[k]+expr[k+1]==='10'){
+                str2=str2+'.';
+            }
+            if (expr[k]+expr[k+1]==='11'){
+                str2=str2+'-';
+            }
+        }
+    
+        for (let n=0; n<Object.values(MORSE_TABLE).length; n++){
+            if(str2===Object.keys(MORSE_TABLE)[n]){
+            str1=str1+Object.values(MORSE_TABLE)[n];
+            }
+        }
+         
+        str2='';
+        }
+    
+    }  
+
+    return(str1);
+
 }
 
 module.exports = {
